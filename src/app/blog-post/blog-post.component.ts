4c6d9aa1cad400fc5e99b12e9c221c22a5a7188b
Brian Brown <brianbrown0417@gmail.com>
2024-09-11 12:49:16 -0400

    fix markdown post

--- src/app/blog-post/blog-post.component.ts
+++ src/app/blog-post/blog-post.component.ts
@@ -3,17 +3,19 @@ import { MarkdownModule } from 'ngx-markdown';
 import { HttpClient } from '@angular/common/http';
 import { MarkdownService } from '../markdown.service';
 import { marked } from 'marked';
+import { Observable, catchError } from 'rxjs'; // Import the Observable class and catchError function from the 'rxjs' library
+import { CommonModule } from '@angular/common';
 
 
 // import { AppRoutingModule } from '../app-routing.module';
-// import { ActivatedRoute, Params } from '@angular/router';
-
+// import { ActivatedRoute, Params } from '@angular/router'
 
 @Component({
   selector: 'app-blog-post',
   standalone: true,
   imports: [
     MarkdownModule,
+    CommonModule
   ],
   templateUrl: './blog-post.component.html',
   styleUrls: ['./blog-post.component.css']
@@ -25,15 +27,20 @@ export class BlogPostComponent implements OnInit {
 
   ngOnInit(): void {
     const markdownUrl = 'https://raw.githubusercontent.com/brianbrown17/git-kt/main/README.md';
-    this.markdownService.getMarkdown(markdownUrl).subscribe(
-      content => {
-        this.markdownContent = content;
-      },
-      error => {
+
+    console.log('Fetching Markdown from', markdownUrl);
+
+    this.markdownService.getMarkdown(markdownUrl).pipe(
+      catchError((error: any) => {
         console.error('Error fetching Markdown', error);
+        return new Observable<any>(); // Return an empty observable to handle the error
+      })
+    ).subscribe(
+      content => {
+        console.log('content fetched', content);
+        this.markdownContent = content;
       }
     );
-    console.log(this.markdownContent);
   }
 }
 
